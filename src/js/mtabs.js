5dbbe70b083150110904d427c16d457c41a7b893
Matt Hall <bizear@gmail.com>
2013-04-23 23:40:19 +0100

    Syntax consistency adjustments

--- src/js/mtabs.js
+++ src/js/mtabs.js
@@ -41,7 +41,7 @@
 			self.tab_names = [];
 
 			// Wrap everything in a container element.
-			self.$wrapper = self.$element.wrapInner('<div class="' + container_class + '" />').find("." + container_class);
+			self.$wrapper = self.$element.wrapInner('<div class="' + container_class + '" />').find('.' + container_class);
 
 			// Wrap all tabs in a container element.
 			self.tabs.wrapAll('<div class="' + opts.tabs_container_class + '" />');
@@ -66,7 +66,7 @@
 				opts = self.options,
 				element = opts.tabsmenu_el,
 				tab_names = self.tab_names,
-				html = "<" + element + ' class="' + opts.tabsmenu_class + '">',
+				html = '<' + element + ' class="' + opts.tabsmenu_class + '">',
 				i = 0,
 				len = tab_names.length,
 				child_node_name,
@@ -78,11 +78,11 @@
 					// Replace any {0} placeholders with any text passed in as arguments.
 					return opts.tmpl.tabsmenu_tab.replace(/\{[0-9]\}/g, function(str) {
 						// Replace non-numeric chars and convert to number.
-						var num = Number(str.replace(/\D/g, ""));
+						var num = Number(str.replace(/\D/g, ''));
 
 						// Return the relevant string from the args array based
 						// on the placeholder number we're currently replacing.
-						return args[num] || "";
+						return args[num] || '';
 					});
 				};
 
@@ -92,7 +92,7 @@
 			}
 
 			// Close the container.
-			html += "</" + element + ">";
+			html += '</' + element + '>';
 
 			// Append it before the element and assign
 			// to the prototype chain for use later.
@@ -100,10 +100,10 @@
 
 			// Get nodeName of the tab menu children
 			// so we can delegate the click event to them.
-			child_node_name = self.$tabs_menu.find(":first")[0].nodeName.toLowerCase();
+			child_node_name = self.$tabs_menu.find(':first')[0].nodeName.toLowerCase();
 
 			// Delegate click evens to each tab.
-			self.$tabs_menu.on("click", child_node_name, function(e) {
+			self.$tabs_menu.on('click', child_node_name, function(e) {
 					var $this = $(this),
 						// Use the tab's index to associate it with it's content.
 						idx = $this.index();
@@ -115,7 +115,7 @@
 					e.preventDefault();
 				})
 				// Select and show the first tab.
-				.find(":first").trigger("click");
+				.find(':first').trigger('click');
 		},
 
 		// Toggle tab passing the relevant index
@@ -125,13 +125,13 @@
 				active_tab_class = opts.active_tab_class;
 
 			// Show the relevant tab content.
-			self.tabs.hide().filter(":eq(" + idx + ")").show();
+			self.tabs.hide().filter(':eq(' + idx + ')').show();
 
 			// Switch tab class names.
-			self.$tabs_menu.children().removeClass(active_tab_class).filter(":eq(" + idx + ")").addClass(active_tab_class);
+			self.$tabs_menu.children().removeClass(active_tab_class).filter(':eq(' + idx + ')').addClass(active_tab_class);
 
 			// Fire callback if defined and current tab has changed.
-			if (typeof opts.onTabSelect === "function" && idx !== self.current_tab) {
+			if (typeof opts.onTabSelect === 'function' && idx !== self.current_tab) {
 				opts.onTabSelect.call(self.element, idx);
 			}
 
@@ -157,7 +157,7 @@
 	$.fn.mtabs = function(options, idx) {
 		return this.each(function() {
 			var $this = $(this),
-				data = $this.data("mtabs"),
+				data = $this.data('mtabs'),
 				opts;
 
 			opts = typeof options === 'object' && options;
@@ -165,7 +165,7 @@
 			// Check if mtabs has already been applied.
 			if (!data) {
 				// Initialise new instance of MattTabs.
-				$this.data("mtabs", (data = new MattTabs(this, opts)));
+				$this.data('mtabs', (data = new MattTabs(this, opts)));
 			}
 
 			// Check for method invocation
@@ -177,12 +177,12 @@
 
 	// Default options.
 	$.fn.mtabs.defaults = {
-		container_class: "tabs", // Specifies class name(s) applied to the overall wrapping element.
-		tabs_container_class: "tabs-content", // Specifies class name(s) applied to tabs content wrapping element.
-		active_tab_class: "active-tab", // Specifies class name for currently active tab.
+		container_class: 'tabs', // Specifies class name(s) applied to the overall wrapping element.
+		tabs_container_class: 'tabs-content', // Specifies class name(s) applied to tabs content wrapping element.
+		active_tab_class: 'active-tab', // Specifies class name for currently active tab.
 		tab_text_el: 'h1, h2, h3, h4, h5, h6', // Specifies element to generate the text from for each tab name.
-		tabsmenu_class: "tabs-menu", // Specifies class name(s) applied to the tabs menu element.
-		tabsmenu_el: "ul", // Specifies element to use as a wrapper for tabs menu items.
+		tabsmenu_class: 'tabs-menu', // Specifies class name(s) applied to the tabs menu element.
+		tabsmenu_el: 'ul', // Specifies element to use as a wrapper for tabs menu items.
 		tmpl: { // Templates used for building HTML structures.
 			tabsmenu_tab: '<li class="tab-{0}"><span>{1}</span></li>'
 		},

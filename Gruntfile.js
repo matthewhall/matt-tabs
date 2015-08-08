4f2ed01933bf2f23bc8d30af1f1028067453366b
Matt Hall <bizear@gmail.com>
2015-08-08 16:45:43 -0700

    Convert tabs to spaces

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,49 +1,52 @@
 module.exports = function (grunt) {
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		jasmine: {
-			src: 'src/js/mtabs.js',
-			options: {
-				vendor: 'src/js/libs/*.js',
-				helpers: 'tests/jasmine/lib/jasmine-jquery.js',
-				specs: 'tests/jasmine/spec/**/*.js'
-			}
-		},
-		jshint: {
-			files: [
-				'Gruntfile.js',
-				'src/**/*.js',
-				'tests/jasmine/spec/**/*.js'
-			],
-			options: {
-				ignores: [
-					'**/libs/*.js',
-					'**/*.min.js'
-				],
-				globals: {
-					window: true,
-					document: true,
-					jQuery: true
-				}
-			}
-		},
-		uglify: {
-			options: {
-				banner: '/*! <%= pkg.title || pkg.name %> v<%= pkg.version %> | <%= pkg.homepage && pkg.homepage %> */\n'
-			},
-			dist: {
-				files: {
-					'src/js/<%= pkg.name %>.<%= pkg.version %>.min.js': ['src/js/mtabs.js']
-				}
-			}
-		}
-	});
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
 
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-contrib-jasmine');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
+    jasmine: {
+      src: 'src/js/mtabs.js',
+      options: {
+        vendor: 'src/js/libs/*.js',
+        helpers: 'tests/jasmine/lib/jasmine-jquery.js',
+        specs: 'tests/jasmine/spec/**/*.js'
+      }
+    },
 
-	grunt.registerTask('test', ['jshint', 'jasmine']);
-	grunt.registerTask('min', ['uglify']);
-	grunt.registerTask('default', ['jshint', 'jasmine', 'uglify']);
+    jshint: {
+      files: [
+        'Gruntfile.js',
+        'src/**/*.js',
+        'tests/jasmine/spec/**/*.js'
+      ],
+      options: {
+        ignores: [
+          '**/libs/*.js',
+          '**/*.min.js'
+        ],
+        globals: {
+          window: true,
+          document: true,
+          jQuery: true
+        }
+      }
+    },
+
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.title || pkg.name %> v<%= pkg.version %> | <%= pkg.homepage && pkg.homepage %> */\n'
+      },
+      dist: {
+        files: {
+          'src/js/<%= pkg.name %>.<%= pkg.version %>.min.js': ['src/js/mtabs.js']
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('test', ['jshint', 'jasmine']);
+  grunt.registerTask('min', ['uglify']);
+  grunt.registerTask('default', ['jshint', 'jasmine', 'uglify']);
 };

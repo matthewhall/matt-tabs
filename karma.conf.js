541704275d26dd972667336954dcf7671f3a8963
Matt Hall <bizear@gmail.com>
2015-08-08 14:43:43 -0700

    Fix path to specs

--- karma.conf.js
+++ karma.conf.js
@@ -14,7 +14,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
         {pattern: 'src/js/mtabs.js', watched: true, served: true, included: true},
-        {pattern: 'tests/spec/*-spec.js', watched: true, served: true, included: true}
+        {pattern: 'tests/spec/*.spec.js', watched: true, served: true, included: true}
     ],
 
 
